5f62736a77b3fb2bd07ca06f02825afce605325f
unknown <anjel9494@gmail.com>
2023-10-07 04:02:14 +0900

    10-07 C# study

---
+++ C#/Entity_FrameWork/Entity_FrameWork/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+namespace Entity_FrameWork
+{
+    public class Category
+    {
+
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public virtual ObservableCollectionListSource<Product> Products { get; set; }
+        // ObservableCollectionListSource<T> 명시적 리스트소스 확장코드 
+        // 원폼에 데이터 바인딩을 하기위해 선언하는 제너릭타입
+    }
+}
