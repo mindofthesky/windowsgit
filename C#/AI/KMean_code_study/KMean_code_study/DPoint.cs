7a080d4de1a301212ce0bf875b3199a87fe601cd
unknown <anjel9494@gmail.com>
2023-10-19 22:16:17 +0900

    10-19 C# study

---
+++ C#/AI/KMean_code_study/KMean_code_study/DPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMean_code_study
+{
+    public class DPoint
+    {
+        public double X {  get; set; }
+        public double Y { get; set; }
+
+        public DPoint()
+        {
+
+        }
+        public DPoint(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
---
+++ C#/AI/KMean_code_study/KMean_code_study/UserState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMean_code_study
+{
+    public class UserState
+    {
+        public double Distortion {  get; set; }
+        public int Index {  get; set; }
+    }
+}
