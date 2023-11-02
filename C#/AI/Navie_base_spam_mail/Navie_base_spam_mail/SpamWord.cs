c1f5adc827b7ea3b3692fe7898405a9b60b92f22
unknown <anjel9494@gmail.com>
2023-11-02 18:06:47 +0900

    AI C# Navie Bayes Spam mail classifier Study 11-02

---
+++ C#/AI/Navie_base_spam_mail/Navie_base_spam_mail/SpamWord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Navie_base_spam_mail
+{
+    public class SpamWord
+    {
+        public static string[] word = new string[] 
+        
+        {
+        "아직도"
+        // 스팸메일 데이터 문자열 넣으면됩니다    
+        };
+    }
+}
---
+++ C#/AI/Navie_base_spam_mail/Navie_base_spam_mail/WordInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Navie_base_spam_mail
+{
+    public class WordInfo
+    {
+        public required int Quantity { get; set; }
+        public double Probability { get; set; }
+    }
+}
