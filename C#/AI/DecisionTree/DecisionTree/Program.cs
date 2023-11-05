7eef5327b8a0099134f798161869a57dd404655f
unknown <anjel9494@gmail.com>
2023-11-05 21:18:17 +0900

    AI C# decision Tree Study 11-05

---
+++ C#/AI/DecisionTree/DecisionTree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace decsion
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateTree(tree);
+        }
+        public static void CreateTree(List<List<string>> tree, int add = 0)
+        {
+            List<string> decisionValues = CreateDecisionValues(tree);
+        }
+        public static List<string> CreateDecisionValues(List<List<string>> tree)
+        {
+            List<string> decisionValues = new List<string>();
+            foreach (List<string> row in tree) if (!decisionValues.Contains(row.LastOrDefault())) decisionValues.Add(row.LastOrDefault());
+            return decisionValues;
+        }
+    }
+   
+}
