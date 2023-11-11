96f0310bd086f365fc7a815c6b26873f116d640d
unknown <anjel9494@gmail.com>
2023-11-11 21:02:53 +0900

    AI C# random forest Study 11-11

---
+++ C#/AI/Random Forest/Random Forest/DesisionNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Forest
+{
+    public class DesisionNode
+    {
+        // 노드 클래스 초기값 선언부 
+        public int Index {  get; set; }
+        public double Threshold {  get; set; }
+        public double InfoGain { get; set; }
+        public DesisionNode? LeftNode {  get; set; }
+        public DesisionNode? RigthNode { get; set; }
+        public int vlaue {  get; set; }
+        public bool IsLeft {  get; set; }
+    }
+}
