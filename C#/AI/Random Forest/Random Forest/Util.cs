c0cf9bf368aeb54240cb4ea49662a28dd584ed11
unknown <anjel9494@gmail.com>
2023-11-16 21:36:31 +0900

    AI C# random forest Study 11-16

---
+++ C#/AI/Random Forest/Random Forest/Util.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Forest
+{
+    public class Util
+    {
+        public static LabeledData[] ReadData(string dir)
+        {
+
+            Dictionary<string, int> labelDict = new Dictionary<string, int>();
+            List<LabeledData> dataList = new List<LabeledData>();
+            using(FileStream steam = File.OpenRead(dir))
+            {
+                using(StreamReader sr = new StreamReader(steam))
+                {
+                    int count = 0;  
+                    
+                    while(sr.EndOfStream)
+                    {
+                        string? line = sr.ReadLine();
+                        if(count == 0 || line is null)
+                        {
+                            count++;
+                            continue;
+                        }
+                        string[] splits = line.Split(',');
+                        int numCol = splits.Length;
+                        if (!labelDict.ContainsKey(splits[numCol - 1])) labelDict.Add(splits[numCol-1], labelDict.Count);
+                        double[] Feature = new double[numCol - 1];
+                        for (int i = 0; i < numCol - 1; i++) _ = double.TryParse(splits[i], out Feature[i]);
+
+                         dataList.Add(new LabeledData(labelDict[splits[numCol - 1]], Feature));
+                    }
+                   
+
+
+                }
+            }
+            return dataList.ToArray();
+        }
+    }
+}
