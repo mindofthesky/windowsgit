7fc685459ee99f4be7ef9758cff9e6121ea6de10
unknown <anjel9494@gmail.com>
2023-11-17 23:03:42 +0900

    AI C# some maybe Markov decision Study 11-17

---
+++ C#/AI/MDP/MDP/Program.cs
@@ -0,0 +1,23 @@
+﻿class mdp 
+{
+
+    public int numstates { get; set; }
+    public int numactions { get; set; }
+    public double gamma { get; set; }
+    public Dictionary<string, double> StateDict;
+    public Dictionary<string, double>[][,] transitionMatrix;
+    public Dictionary<string, double>[] ActionProbability;
+    public double[] Jest;
+
+    public mdp(int numstates, int numactions, double gamma)
+    {
+        this.numstates = numstates;
+        this.numactions = numactions;
+        this.gamma = gamma;
+        StateDict = new Dictionary<string, double>();
+        transitionMatrix = new Dictionary<string, double>[numactions][,];
+        ActionProbability = new Dictionary<string, double>[numactions];
+        Jest = new double[numactions];
+        for (int i = 0; i < numactions; i++) transitionMatrix[i] = new Dictionary<string, double>[numstates, numstates];
+    }
+}
