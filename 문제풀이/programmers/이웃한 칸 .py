55a16389bacdcfac00d1f35c7565bfd7a1fcd461
unknown <anjel9494@gmail.com>
2024-02-14 01:45:09 +0900

    progarmmers codetest study 24-02-14

---
+++ 문제풀이/programmers/이웃한 칸 .py
@@ -0,0 +1,26 @@
+board = [["blue", "red", "orange", "red"], ["red", "red", "blue", "orange"], ["blue", "orange", "red", "red"], ["orange", "orange", "red", "blue"]]
+board1 = [["yellow", "green", "blue"], ["blue", "green", "yellow"], ["yellow", "blue", "blue"]]
+h = 1
+w = 1
+def solution(board, h, w):
+    answer = 0
+    # dh dw [0,1] [-1,0], [1,0],[0,-1] = BFS 동서남북
+    m = len(board)
+    dh, dw = [0,1,-1,0],[1,0,0,-1]
+    h_check, w_check = 0,0
+    
+    for i in range(len(dw)):
+        h_check = h + dh[i]
+        w_check = w + dw[i]
+        #print(h_check,w_check)
+        if (0 <= h_check <= len(board)-1) and (0 <= w_check <= len(board[0])-1):
+            answer+=int(board[h][w] == board[h_check][w_check])
+
+            
+    print(board[h][w])
+    print(answer)
+    # dfs를 알면 이미 값을 다줬다
+    # 효율성 그냥 생각안해도되는문제입니다 
+    # 시간제한이없거든요 
+    return answer
+solution(board, h, w)
