1aad9a9b7e5b8f3f675d7bc12087b84084e7d81b
unknown <anjel9494@gmail.com>
2024-04-18 19:11:37 +0900

    progarmmers codetest study 24-04-15 다시 15일 누락 ,16일차

--- 문제풀이/programmers/할인행사.py
+++ 문제풀이/programmers/할인행사.py
@@ -4,4 +4,13 @@ Created on Sat Jan 27 20:17:27 2024
 
 @author: mindo
 """
+want = ["banana", "apple", "rice", "pork", "pot"]	
+number = [3, 2, 2, 2, 1]
+discount = ["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"]
 
+def solution(want, number, discount):
+    answer = 0
+    print(aa )
+    return answer
+
+solution(want, number, discount)
