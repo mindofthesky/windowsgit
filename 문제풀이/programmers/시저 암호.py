415b4e8161d830d5dbe2bd67bc8e4439682d7a59
unknown <anjel9494@gmail.com>
2024-05-07 22:51:32 +0900

    progarmmers codetest study 24-05-07 일단1달차

---
+++ 문제풀이/programmers/시저 암호.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 22:48:15 2024
+
+@author: mindo
+"""
+s ="AB"
+n = 1
+def solution(s, n):
+    answer = ''
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i]=chr((ord(s[i])- ord("A")+n)%26 + ord("A"))
+        elif s[i].islower():
+            s[i]=chr((ord(s[i]) - ord("a")+n)%26 + ord("a"))
+    answer = "".join(s)        
+    return answer
+solution(s, n)
